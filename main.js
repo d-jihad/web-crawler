caa64d6fcb499416426948ff5dbd222caa067f3d
Djihad Boukara <djihad.boukara@proton.me>
2024-06-11 11:28:28 +0200

    created main function with args checking

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-import { getURLsFromHTML } from "./crawl";
+function main() {
+  const args = process.argv.length;
+
+  if (args != 3) {
+    console.log("Wrong arguments number");
+    console.log("Usage: npm run start BASE_URL");
+    return;
+  }
+
+  const inputUrl = process.argv[2];
+  let baseURL;
+
+  try {
+    baseURL = new URL(inputUrl);
+  } catch (error) {
+    console.log("The input is not a valid url");
+    return;
+  }
+
+  console.log(`Using ${baseURL} as start`);
+}
+
+main();
